ff9f34abb4593d422d5682c5db920eec6d67aafc
alifsuhendri <89438996+alifsuhendri@users.noreply.github.com>
2021-08-25 18:00:43 +0700

    Add files via upload

---
+++ Latihan_halaman_22.m
@@ -0,0 +1,80 @@
+>> % 1. Hitunglah dengan MATLAB:
+>> 12 / 3.5
+ans = 3.4286
+>> (3 + 5/4)^2
+ans = 18.062
+>> (0.25^2 + 0.75^2)^(1/2)
+ans = 0.7906
+>> 2 /(6/0.3)
+ans = 0.1000
+
+>> % 2. Buatlah empat variabel berikut:
+>> A = 25;
+>> B = 50;
+>> C = 125;
+>> D = 89;
+>> % Hitunglah dan simpan dalam variabel baru:
+>> X = A + B + C
+X = 200
+>> Y = A /(D + B)
+Y = 0.1799
+>> Z = D^(A/B) + C
+Z = 134.43
+
+>> % 3. Manakah di antara nama-nama variabel berikut yang valid ?
+>> % luas -- valid
+>> % kel_1 -- valid
+>> % Time -- valid
+>> % time_from_start -- valid
+
+ % 4. Misalkan x = pi/6, y = 0,001; hitunglah:
+>> x = pi / 6;
+>> y = 0.001;
+>> sqrt(y)
+ans = 0.031623
+>> exp(-x)
+ans = 0.5924
+>> sin(x)
+ans = 0.5000
+ cos(2*x)
+ans = 0.5000
+>> tan(3*x)
+ans = 1.6331e+16
+>> log10(y)
+ans = -3
+>> log2(y)
+ans = -9.9658
+>> log(y)
+ans = -6.9078
+
+>> % 5. Misalkan p = 9+16i dan q = -9+16i; hitunglah:
+>> p = 9 + 16i;
+>> q = -9 + 16i;
+>> r = p * q
+r = -337
+>> s = p / q
+s =  0.5193 - 0.8546i
+>> p - r
+ans =  346 +  16i
+>> r + s
+ans = -336.4807 -   0.8546i
+>> p^2
+ans = -175 + 288i
+>> sqrt(q)
+ans =  2.1630 + 3.6985i
+>> abs(p)
+ans = 18.358
+>> angle(p)
+ans = 1.0584
+>> abs(q)
+ans = 18.358
+>> angle(q)
+ans = 2.0832
+>> abs(r)
+ans = 337
+>> angle(r)
+ans = 3.1416
+>> abs(s)
+ans = 1
+>> angle(s)
+ans = -1.0248
